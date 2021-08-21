c8ebec9519ed9c61704d626a215668e2c6cece4b
Alex <arcani91@gmail.com>
2021-08-22 01:38:49 +0300

    Added methods to change the slider values

--- Homework_2_ChangingColors/ViewController.swift
+++ Homework_2_ChangingColors/ViewController.swift
@@ -13,17 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet var colorBox: UIView!
     
     // Red Color Stack Outlets
-    
     @IBOutlet var redColorValue: UILabel!
     @IBOutlet var redSlider: UISlider!
     
     // Green Color Stack Outlets
-    
     @IBOutlet var greenColorValue: UILabel!
     @IBOutlet var greenSlider: UISlider!
     
     // Blue Color Stack Outlets
-    
     @IBOutlet var blueColorValue: UILabel!
     @IBOutlet var blueSlider: UISlider!
     
@@ -31,18 +28,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Default Slider Positions
-        
         redSlider.value = 0.5
         greenSlider.value = 0.5
         blueSlider.value = 0.5
         
         // Default Shown Slider Values
-        
         redColorValue.text = String(format: "%.2f", redSlider.value)
         greenColorValue.text = String(format: "%.2f", greenSlider.value)
         blueColorValue.text = String(format: "%.2f", blueSlider.value)
     }
 
-
+    override func viewDidLayoutSubviews() {
+        colorBox.layer.cornerRadius = 10
+    }
+    
+    @IBAction func changeShownRedSliderValue() {
+        redColorValue.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func changeShownGreenSliderValue() {
+        greenColorValue.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func changeShownBlueSliderValue() {
+        blueColorValue.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
