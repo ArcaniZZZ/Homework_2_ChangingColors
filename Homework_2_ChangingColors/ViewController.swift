3250da9c0c2dbcbd749e870ff331e10a6dc2b6dc
Alex <arcani91@gmail.com>
2021-08-22 00:54:37 +0300

    Still setting up the UI

--- Homework_2_ChangingColors/ViewController.swift
+++ Homework_2_ChangingColors/ViewController.swift
@@ -8,10 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Color Box Labels
+    @IBOutlet var colorBox: UIView!
+    
+    // Red Color Stack Outlets
+    
+    @IBOutlet var redColorValue: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    
+    // Green Color Stack Outlets
+    
+    @IBOutlet var greenColorValue: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    
+    // Blue Color Stack Outlets
+    
+    @IBOutlet var blueColorValue: UILabel!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Default Slider Positions
+        
+        redSlider.value = 0.5
+        greenSlider.value = 0.5
+        blueSlider.value = 0.5
+        
+        // Default Shown Slider Values
+        
+        redColorValue.text = String(format: "%.2f", redSlider.value)
+        greenColorValue.text = String(format: "%.2f", greenSlider.value)
+        blueColorValue.text = String(format: "%.2f", blueSlider.value)
     }
 
 
