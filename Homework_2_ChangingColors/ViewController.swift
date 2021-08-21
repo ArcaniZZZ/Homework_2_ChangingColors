464a17bbc336fa4d1482d68858cf4fe90aefd455
Alex <arcani91@gmail.com>
2021-08-21 23:16:28 +0300

    Initial Commit

---
+++ Homework_2_ChangingColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2_ChangingColors
+//
+//  Created by Arcani on 21.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
